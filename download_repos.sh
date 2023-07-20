88fe9976f7d5d1734b1ef0929b3933928bc2419f
Louis Tremblay Thibault <tremblaythibaultl@gmail.com>
2023-07-20 17:19:27 +0200

    add submodules

---
+++ download_repos.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+git clone https://github.com/zama-ai/go-ethereum
+git clone https://github.com/zama-ai/evmos
+git clone https://github.com/zama-ai/ethermint
+git clone https://github.com/zama-ai/fhevm-requires-db
